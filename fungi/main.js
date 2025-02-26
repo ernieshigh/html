b9b4fc1e59428b6048efd0f1d8fc724a82893603
ernie <erniehightower@gmail.com>
2025-02-26 11:17:02 +0800

    add mobile button

---
+++ fungi/main.js
@@ -0,0 +1,14 @@
+const navToggle = document.querySelector("[aria-controls=main-nav]");
+const mainNav = document.querySelector(".main-nav");
+
+navToggle.addEventListener("click", () => {
+  const navOpen = navToggle.getAttribute("aria-expanded");
+
+  if (navOpen === "false") {
+    navToggle.setAttribute("aria-expanded", "true");
+  } else {
+    navToggle.setAttribute("aria-expanded", "false");
+  }
+
+  console.log(navOpen);
+});
